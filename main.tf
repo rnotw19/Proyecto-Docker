de5b6a6e1055b3d973119475957dc92009b83f3a
rnotw19 <ronmel.bautista@unitec.edu>
2022-06-21 23:17:13 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,43 +13,23 @@ provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
+resource "aws_launch_template" "foobar" {
 
-variable "my_access_key" {
-  description = "Access-key-for-AWS"
-  default = "no_access_key_value_found"
-}
- 
-variable "my_secret_key" {
-  description = "Secret-key-for-AWS"
-  default = "no_secret_key_value_found"
-}
- 
-output "access_key_is" {
-  value = var.my_access_key
-}
- 
-output "secret_key_is" {
-  value = var.my_secret_key
-}
- 
-provider "aws" {
-	region = "eu-west-3"
-	access_key = var.my_access_key
-	secret_key = var.my_secret_key
-}
-
-resource "aws_instance" "app_server" {
-count = 2
-  ami           = "ami-0022f774911c1d690"
+  name = "launch-terraform-ron"
+  description = "Launch template creado con terraform"
+  security_group_names = ["SG_AWS_Terraform"]
+  key_name ="LLave-Ronmel"
+  image_id      = "ami-0022f774911c1d690"
   instance_type = "t2.micro"
-  key_name = "LLave-Ronmel"
-  security_groups = ["launch-wizard-6-Kelvin"]
-  user_data = "${file("Userdata.sh")}"
-
   tags = {
-    Name = "Terraform-Chat-app"
+    Estudiante = "Ronmel"
+    Name = "Tarea-Terraform_user"
   }
+  
+   user_data = "${filebase64("Userdata.sh")}"
 }
+
+
 resource "aws_lb" "test" {
   name               = "LB-APPmario"
   internal           = false
