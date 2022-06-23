515b5298d3bd8ac33d9700e1b2dcddf78b075b0a
rnotw19 <ronmel.bautista@unitec.edu>
2022-06-22 23:45:26 -0600

    Removed variables
    variables removed and some unnecessary Info

--- main.tf
+++ main.tf
@@ -13,6 +13,64 @@ provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
+
+
+resource "aws_security_group" "Allow_all" {
+  name        = "SG_AWS_Pmario"
+  description = "grupo de seguridad creado con terraform"
+  
+
+  ingress {
+    
+    from_port        = 8600
+    to_port          = 8600
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+     ingress {
+    
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Nuevo_SG"
+  }
+}
+
+
+resource "aws_autoscaling_group" "bar" {
+ name = "AG-terraform-ron"
+  availability_zones = ["us-east-1a","us-east-1b","us-east-1c","us-east-1d","us-east-1e","us-east-1f"]
+  desired_capacity   = 1
+  max_size           = 4
+  min_size           = 1
+
+  launch_template {
+    id      = aws_launch_template.foobar.id
+    version = "$Latest"
+  }
+  tags = [
+    {
+      key                 = "Name"
+      value               = "Pro_Mario_Ronmel"
+      propagate_at_launch = true
+    },
+  ]
+}
 resource "aws_launch_template" "foobar" {
 
   name = "launch-terraform-ron"
@@ -34,18 +92,15 @@ resource "aws_lb" "test" {
   name               = "LB-APPmario"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [for subnet in aws_subnet.public : subnet.id]
+  security_groups    = [aws_security_group.Allow_all.id]
+  subnets            = [data.aws_availability_zones.available.id]
 
   enable_deletion_protection = true
 
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.bucket
-    prefix  = "test-lb"
-    enabled = true
-  }
-
   tags = {
-    Environment = "production"
-  }
+    Name  = "AWS_LB"
+  Environment = "production"
+     }
+
 }
+
--- terraform.tfvars
+++
@@ -1,2 +0,0 @@
-my_access_key = "RANDOM_TEST_ACCESS_KEY"
-my_secret_key = "RANDOM_TEST_SECRET_KEY"
